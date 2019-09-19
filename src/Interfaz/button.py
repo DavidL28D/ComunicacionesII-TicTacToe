b9ed0c2472b132eeb96796aceb01a17d15024d5d
Yeison Fuentes <yeisonf15@gmail.com>
2019-09-19 19:57:22 -0400

    Version inicial del menú

--- src/Interfaz/button.py
+++ src/Interfaz/button.py
@@ -1,11 +1,19 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QPushButton
 
 class Button(QPushButton):
+    height=60
     width = 100
-    heigth = 30
-    def __init__(self, name, posx, posy,QMainWindow):
-        super().__init__(self)
-        self.setGeometry(self.width,self.heigth)
-        self.move(posx,posy)
+    def __init__(self,label,window):
+        super().__init__()        
+        self.initParameters(100,500,label,window)
 
+    def initParameters(self,posx,posy,label,window):
+        self.setFixedHeight(self.height)
+        self.setFixedWidth(self.width)
+        self.setText(label)
+        self.show()
+        if label == "Play":
+            self.clicked.connect(window.play)
+        if label == "Join":
+            self.clicked.connect(window.join)
--- src/Interfaz/window.py
+++ src/Interfaz/window.py
@@ -1,36 +1,58 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import *
 from Servidor.servidor import server
 from Cliente.cliente import client
 from Interfaz.button import Button
 class Window(QMainWindow):
-
-	def __init__(self):
-		
+	
+	def __init__(self):		
 		super().__init__()
 		self.width = 800
 		self.height = 600
 		self.initUI()
 		
 	def initUI(self):
-		#playBtn = Button('Play',100,30,self)
-		#playBtn.clicked.connect(self.play)
-		playBtn = QPushButton('Play',self)
-		playBtn.resize(100,30)
-		playBtn.move(50,350)
-		playBtn.clicked.connect(self.play)
-
-		joinBtn = QPushButton('Join',self)
-		joinBtn.resize(100,30)
-		joinBtn.move(200,350)
-		joinBtn.clicked.connect(self.join)
 		self.setGeometry(100,100,self.width,self.height)
 		self.setWindowTitle("TicTacToe")
-		
+		self.createButtons()
+		self.createTitle()
+		self.createHorizontalLayout()
+		self.createVerticalLayout()
+		home = QWidget()
+		home.setLayout(self.vbox)		
+		self.setCentralWidget(home)	
 		self.show()
 
 	def play(self):
-		server()
+		server()		
 
 	def join(self):
 		client()
+
+	def createButtons(self):
+		self.playBtn = Button('Play',self)
+		self.joinBtn = Button('Join',self)
+
+	def createTitle(self):
+		self.title = QLabel()
+		self.title.setText("TicTacToe")
+		self.title.setFont(QtGui.QFont("Times", 40, QtGui.QFont.Bold))
+		self.title.setAlignment(Qt.AlignCenter)
+	
+	def createHorizontalLayout(self):
+		self.hbox = QHBoxLayout()	
+		self.hbox.addStretch(1)
+		self.hbox.addWidget(self.playBtn)		
+		self.hbox.addStretch(1)
+		self.hbox.addWidget(self.joinBtn)	
+		self.hbox.addStretch(1)
+
+	def createVerticalLayout(self):
+		self.vbox = QVBoxLayout()
+		self.vbox.addStretch(2)
+		self.vbox.addWidget(self.title)
+		self.vbox.addStretch(2)
+		self.vbox.addLayout(self.hbox)	
+		self.vbox.addStretch(4)		
