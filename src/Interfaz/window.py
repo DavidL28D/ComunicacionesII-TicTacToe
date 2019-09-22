b478383f9a9f6982bc54684fc4d4785370ff9ff6
Yeison Fuentes <yeisonf15@gmail.com>
2019-09-21 22:43:34 -0400

    Cambio entre ventanas y creada la ventana de juego

--- src/Interfaz/window.py
+++ src/Interfaz/window.py
@@ -1,10 +1,13 @@
 import sys
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5 import QtGui 
+from PyQt5.QtWidgets import QListWidget, QStackedWidget, QApplication, QLabel, QWidget, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout
 from PyQt5.QtCore import *
 from Servidor.servidor import server
 from Cliente.cliente import client
 from Interfaz.button import Button
+from Interfaz.menu import MenuWidget
+from Interfaz.game import GameWidget
+
 class Window(QMainWindow):
 	
 	def __init__(self):		
@@ -16,43 +19,27 @@ class Window(QMainWindow):
 	def initUI(self):
 		self.setGeometry(100,100,self.width,self.height)
 		self.setWindowTitle("TicTacToe")
-		self.createButtons()
-		self.createTitle()
-		self.createHorizontalLayout()
-		self.createVerticalLayout()
-		home = QWidget()
-		home.setLayout(self.vbox)		
-		self.setCentralWidget(home)	
+		#Creacion del Widget Apilado
+		self.windows = QStackedWidget(self)	
+
+		#Creacion de los items a apilar
+		self.menu = MenuWidget(self)
+		self.game = GameWidget()
+
+		#Agregar los widgets al Widget apilado
+		self.windows.addWidget(self.menu)
+		self.windows.addWidget(self.game)
+
+		self.setCentralWidget(self.windows)
+			
 		self.show()
 
 	def play(self):
-		server()		
+		self.windows.setCurrentIndex(1)
+		print ("Play")
+		#server()
+				
 
 	def join(self):
-		client()
-
-	def createButtons(self):
-		self.playBtn = Button('Play',self)
-		self.joinBtn = Button('Join',self)
-
-	def createTitle(self):
-		self.title = QLabel()
-		self.title.setText("TicTacToe")
-		self.title.setFont(QtGui.QFont("Times", 40, QtGui.QFont.Bold))
-		self.title.setAlignment(Qt.AlignCenter)
-	
-	def createHorizontalLayout(self):
-		self.hbox = QHBoxLayout()	
-		self.hbox.addStretch(1)
-		self.hbox.addWidget(self.playBtn)		
-		self.hbox.addStretch(1)
-		self.hbox.addWidget(self.joinBtn)	
-		self.hbox.addStretch(1)
-
-	def createVerticalLayout(self):
-		self.vbox = QVBoxLayout()
-		self.vbox.addStretch(2)
-		self.vbox.addWidget(self.title)
-		self.vbox.addStretch(2)
-		self.vbox.addLayout(self.hbox)	
-		self.vbox.addStretch(4)		
+		#client()
+		pass
